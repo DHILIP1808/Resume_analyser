0650691880fbf3fd2d6eaa300f52da62648297a8
dhilip <dkdhilip777@gmail.com>
2025-11-07 13:29:52 +0530

    changed backend url for deploying

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,5 +1,7 @@
 // services/api.js
-const API_BASE_URL = 'http://localhost:8000';
+// eslint-disable-next-line no-undef
+const API_BASE_URL = process.env.REACT_APP_API_URL;
+
 
 // Headers for JSON requests
 const jsonHeaders = {
